1d9568dedabc9fd67542024297d23c217a85936d
Anson Baby <ansonbm@gmail.com>
2015-03-01 10:16:29 -0500

    Added internal documentation

--- Comp123_Assignment03_AnsonBaby/Program.cs
+++ Comp123_Assignment03_AnsonBaby/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Assignment-03_Comp123
+ * Name : Anson Baby
+ * Date : 26 Feb 2015
+ * App Description : Build and impliment planet class
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +16,25 @@ namespace Comp123_Assignment03_AnsonBaby
     {
         static void Main()
         {
-            GiantPlanet
+
+            GiantPlanet jupiter = new GiantPlanet ("gas","Jupiter",10000000000,9999999999);
+            Console.WriteLine(jupiter.ToString());
+            Console.WriteLine();
+            TerrestrialPlanet earth = new TerrestrialPlanet(true, "Earth", 55555555, 7777777777);
+            Console.WriteLine(earth.ToString());
+
+            waitForAnyKey();
+
+        }
+
+         // PRIVATE METHODS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void waitForAnyKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+       
         }
     }
 }
