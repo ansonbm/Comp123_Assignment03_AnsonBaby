0273552bd61823a600eb65fa19cbe2070ed307c1
Anson Baby <ansonbm@gmail.com>
2015-03-01 10:02:50 -0500

    Added Interface IHasMoons

---
+++ Comp123_Assignment03_AnsonBaby/IHasMoons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_Assignment03_AnsonBaby
+{
+    interface IHasMoons
+    {
+        // PUBIC METHODS++++++++++++++++++++++++++++++++++++++++++++++++L
+        bool HasMoons();
+    }
+}
