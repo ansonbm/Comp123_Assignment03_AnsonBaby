4915e4df99a82bc3e4af1fcfafb221af959093ca
Anson Baby <ansonbm@gmail.com>
2015-03-01 09:55:51 -0500

    Added GiantPlanet Subclass

---
+++ Comp123_Assignment03_AnsonBaby/GiantPlanet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_Assignment03_AnsonBaby
+{
+    class GiantPlanet : Planet
+    {
+ //PRIVATE INSTANCE VARIABLES++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private string _type;
+
+
+        //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public GiantPlanet(string type, string name, double diameter, double mass)
+            : base(name, diameter, mass)
+        {
+            this._type = type;
+        }
+
+
+        //PUBLIC METHODS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public bool HasMoons()
+        {
+            if (MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasRings()
+        {
+            if (RingCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    
+    }
+}
